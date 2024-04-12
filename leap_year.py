3f50e086e5422812903e5e9f6511472ecedb66ce
Agusprieto05 <162721778+Agusprieto05@users.noreply.github.com>
2024-04-12 12:54:30 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,6 @@
 def leap_year():
-    año = int(input("Ingrese un año: "))
+    year = int(input("Ingrese un año: "))
     
-    result = (not año % 400) or (not año % 4 and año % 100)
+    result = (not year % 400) or (not year % 4 and year % 100)
     
-    print( f" El año {año}{""if result else "no "}es bisiesto")
+    print( f" El año {year}{""if result else "no "}es bisiesto")
